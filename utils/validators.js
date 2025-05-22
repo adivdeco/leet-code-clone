84d47649f165697fe60786fae6830fa0a3793cbd
adivdeco <sadiv120@gmail.com>
2025-05-23 00:00:38 +0530

    check

--- utils/validators.js
+++ utils/validators.js
@@ -4,7 +4,7 @@ function validateuser (data) {
  
     const mendatoryFields = ['name', 'email', 'password'];
     const isvalied = mendatoryFields.every((field) => {
-        return data[field] !== undefined && data[field] !== null && data[field] !== '';
+        return data[field] !== undefined && data[field] !== null && data[field] !== ''; // check if the field is not undefined, null or empty string
     })
     // const isvalied = mendatoryFields.every( (field) => Object.keys(data).includes(field) )
     if(!isvalied){
@@ -14,3 +14,9 @@ function validateuser (data) {
 }
 
 module.exports = validateuser;
+const arr = ['name', 'email', 'password'];
+
+if(arr.includes('name')){
+    console.log('yes');
+}
+//here we are checking if the data object has all the mendatory fields
